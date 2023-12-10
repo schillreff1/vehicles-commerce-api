8c0d987a1d0f762cf79f790cd0e1dcc87c31549d
Leandro Schillreff <leandroschillreff@hotmail.com>
2023-12-10 17:09:10 -0400

    Initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  const options = new DocumentBuilder()
+    .setTitle('Vehicles Commerce')
+    .setDescription('Api')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
